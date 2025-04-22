db27d9069c04aa78625b7ef26b8ff420a9299a36
Anton Patsev <patsev.anton@gmail.com>
2025-04-22 16:44:43 +0600

    Бесконечный цикл с проверками каждые 5 секунд

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-    "log"
-    "github.com/joho/godotenv"
+    "fmt"
+	"log"
+	"time"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatalf("Ошибка загрузки .env файла: %v", err)
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки .env файла: %v", err)
+	}
 
-    cfg, err := LoadConfig()
-    if err != nil {
-        log.Fatalf("Ошибка загрузки конфигурации: %v", err)
-    }
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
+	}
 
-    CheckHosts(cfg)
+	// Бесконечный цикл с проверками каждые 5 секунд
+	for {
+		CheckHosts(cfg)
+		time.Sleep(5 * time.Second)
+		fmt.Println() // Пустая строка между выводами
+	}
 }
