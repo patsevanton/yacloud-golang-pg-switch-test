d9026584a353ea6a871de5de05c11eaf5b9d3898
Anton Patsev <patsev.anton@gmail.com>
2025-04-22 13:10:01 +0600

    Проверка роли для FQDN

--- checker.go
+++ checker.go
@@ -1,42 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "context"
+	"fmt"
+	"log"
+	"context"
+	"strings"
 )
 
 var ctx = context.Background()
 
 func CheckHosts(cfg *Config) {
-    fmt.Println("Проверка роли для FQDN:", cfg.ClusterFQDN)
-    conn, err := ConnectToHost(cfg, cfg.ClusterFQDN)
-    if err != nil {
-        log.Printf("[FQDN] Ошибка подключения: %v\n", err)
-        return
-    }
-    defer conn.Close(ctx)
+	fmt.Println("Проверка роли для FQDN:", cfg.ClusterFQDN)
+	conn, err := ConnectToHost(cfg, cfg.ClusterFQDN)
+	if err != nil {
+		log.Printf("[FQDN] Ошибка подключения: %v\n", err)
+	} else {
+		defer conn.Close(ctx)
+		role, err := GetRole(conn)
+		if err != nil {
+			log.Printf("[FQDN] Ошибка определения роли: %v\n", err)
+		} else {
+			fmt.Printf("[FQDN] Роль: %s\n", role)
+		}
+	}
 
-    role, err := GetRole(conn)
-    if err != nil {
-        log.Printf("[FQDN] Ошибка определения роли: %v\n", err)
-    } else {
-        fmt.Printf("[FQDN] Роль: %s\n", role)
-    }
+	for _, host := range cfg.Hosts {
+		conn, err := ConnectToHost(cfg, host)
+		if err != nil {
+			if strings.Contains(err.Error(), "read only connection") {
+				// Пропускаем вывод для read-only реплик
+				continue
+			}
+			log.Printf("[ХОСТ %s] Ошибка подключения: %v\n", host, err)
+			continue
+		}
+		defer conn.Close(ctx)
 
-    for _, host := range cfg.Hosts {
-        conn, err := ConnectToHost(cfg, host)
-        if err != nil {
-            log.Printf("[ХОСТ %s] Ошибка подключения: %v\n", host, err)
-            continue
-        }
-        defer conn.Close(ctx)
-
-        role, err := GetRole(conn)
-        if err != nil {
-            log.Printf("[ХОСТ %s] Ошибка определения роли: %v\n", host, err)
-        } else {
-            fmt.Printf("[ХОСТ %s] Роль: %s\n", host, role)
-        }
-    }
+		role, err := GetRole(conn)
+		if err != nil {
+			log.Printf("[ХОСТ %s] Ошибка определения роли: %v\n", host, err)
+		} else {
+			fmt.Printf("[ХОСТ %s] Роль: %s\n", host, role)
+		}
+	}
 }
