456d165ce3640954be17acf4357d08fcd155fd42
Anton Patsev <patsev.anton@gmail.com>
2025-04-22 13:04:22 +0600

    add ssl

--- config.go
+++ config.go
@@ -1,30 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"fmt"
+	"os"
+	"strings"
 )
 
 type Config struct {
-    PGUser      string
-    PGPassword  string
-    PGDB        string
-    ClusterFQDN string
-    Hosts       []string
+	PGUser      string
+	PGPassword  string
+	PGDB        string
+	ClusterFQDN string
+	Hosts       []string
 }
 
 func LoadConfig() (*Config, error) {
-    hostsRaw := os.Getenv("HOSTS")
-    if hostsRaw == "" {
-        return nil, fmt.Errorf("переменная HOSTS не задана")
-    }
+	hostsRaw := os.Getenv("HOSTS")
+	if hostsRaw == "" {
+		return nil, fmt.Errorf("переменная HOSTS не задана")
+	}
 
-    return &Config{
-        PGUser:      os.Getenv("PG_USER"),
-        PGPassword:  os.Getenv("PG_PASSWORD"),
-        PGDB:        os.Getenv("PG_DB"),
-        ClusterFQDN: os.Getenv("CLUSTER_FQDN"),
-        Hosts:       strings.Split(hostsRaw, ","),
-    }, nil
+	// Очистка хостов от пробелов
+	hosts := strings.Split(hostsRaw, ",")
+	for i, host := range hosts {
+		hosts[i] = strings.TrimSpace(host)
+	}
+
+	return &Config{
+		PGUser:      os.Getenv("PG_USER"),
+		PGPassword:  os.Getenv("PG_PASSWORD"),
+		PGDB:        os.Getenv("PG_DB"),
+		ClusterFQDN: strings.TrimSpace(os.Getenv("CLUSTER_FQDN")),
+		Hosts:       hosts,
+	}, nil
 }
--- dbclient.go
+++ dbclient.go
@@ -1,24 +1,53 @@
 package main
 
 import (
-    "fmt"
-    "github.com/jackc/pgx/v5"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func ConnectToHost(cfg *Config, host string) (*pgx.Conn, error) {
-    dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=require",
-        cfg.PGUser, cfg.PGPassword, host, cfg.PGDB)
-    return pgx.Connect(ctx, dsn)
+	// Загрузка CA сертификата
+	caCert, err := os.ReadFile("/home/user/.postgresql/root.crt") // Укажите правильный путь к CA cert
+	if err != nil {
+		return nil, fmt.Errorf("unable to read CA cert: %v", err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	if !caCertPool.AppendCertsFromPEM(caCert) {
+		return nil, fmt.Errorf("failed to add CA cert to pool")
+	}
+
+	// Формирование DSN
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:6432/%s?sslmode=verify-full&target_session_attrs=read-write",
+		cfg.PGUser, cfg.PGPassword, host, cfg.PGDB)
+
+	// Парсинг конфигурации
+	connConfig, err := pgx.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse config: %v", err)
+	}
+
+	// Настройка TLS
+	connConfig.TLSConfig = &tls.Config{
+		RootCAs:    caCertPool,
+		ServerName: host, // Важно для проверки сертификата
+	}
+
+	return pgx.ConnectConfig(ctx, connConfig)
 }
 
 func GetRole(conn *pgx.Conn) (string, error) {
-    var isInRecovery bool
-    err := conn.QueryRow(ctx, "SELECT pg_is_in_recovery()").Scan(&isInRecovery)
-    if err != nil {
-        return "", err
-    }
-    if isInRecovery {
-        return "replica", nil
-    }
-    return "master", nil
+	var isInRecovery bool
+	err := conn.QueryRow(ctx, "SELECT pg_is_in_recovery()").Scan(&isInRecovery)
+	if err != nil {
+		return "", err
+	}
+	if isInRecovery {
+		return "replica", nil
+	}
+	return "master", nil
 }
