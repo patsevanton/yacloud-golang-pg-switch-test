3987604e39677b15a78e25b0edf32e08be3ad6e9
Anton Patsev <patsev.anton@gmail.com>
2025-05-08 20:38:50 +0600

    fix line Проверка

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	}
 
 	for {
-		fmt.Printf("\n=== Проверка %s ===\n", time.Now().Format("2006-01-02 15:04:05"))
+		fmt.Printf("\n=== Проверка %s ===", time.Now().Format("2006-01-02 15:04:05"))
 		fmt.Println()
 		CheckClusterFQDN(cfg)
 		time.Sleep(5 * time.Second)
