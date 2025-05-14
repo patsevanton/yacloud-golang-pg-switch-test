f3d0c571fc5b1275848dcd07959e9cde926a714a
Anton Patsev <patsev.anton@gmail.com>
2025-05-14 17:13:12 +0600

    add target_session_attrs to dsn

--- main.go
+++ main.go
@@ -75,21 +75,12 @@ func CreateConnectionPool(cfg *Config, host string) (*pgxpool.Pool, error) {
 		return nil, fmt.Errorf("failed to add CA cert to pool")
 	}
 
-	params := url.Values{}
-	if cfg.PGSSLMode != "" {
-		params.Set("sslmode", cfg.PGSSLMode)
-	}
-	if cfg.PGTargetSessionAttrs != "" {
-		params.Set("target_session_attrs", cfg.PGTargetSessionAttrs)
-	}
-
 	dsn := fmt.Sprintf(
-		"postgres://%s:%s@%s:6432/%s?%s&pool_max_conn_lifetime=1h&pool_max_conn_idle_time=30m&target_session_attrs=any",
+		"postgres://%s:%s@%s:6432/%s?%s&pool_max_conn_lifetime=1h&pool_max_conn_idle_time=30m&sslmode=disable&target_session_attrs=any",
 		url.QueryEscape(cfg.PGUser),
 		url.QueryEscape(cfg.PGPassword),
 		host,
 		url.PathEscape(cfg.PGDatabase),
-		params.Encode(),
 	)
 
 	config, err := pgxpool.ParseConfig(dsn)
